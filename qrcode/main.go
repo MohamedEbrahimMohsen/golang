4c8e4342d1e2d085f6e2d5bda011416b57ea9186
Mohamed Mohsen <m.mohsen@dell.com>
2023-01-30 23:59:54 +0200

    integrated with postgres db.

--- qrcode/main.go
+++ qrcode/main.go
@@ -5,45 +5,74 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/skip2/go-qrcode"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
 func main() {
+	if err := connectDB(); err != nil {
+		panic(err)
+	}
+
 	r := gin.Default()
 	r.POST("/qr", generate)
 	r.Run("localhost:8000")
 }
 
 type qrModel struct {
-	URL   string `json:"url"`
-	Title string `json:"title"`
+	ID     uint   `json:"id" gorm:"primaryKey"`
+	URL    string `json:"url"`
+	Title  string `json:"title"`
+	QRCode []byte `json:"qrcode"`
 }
 
 func generate(c *gin.Context) {
-	var qr qrModel
+	var model qrModel
 
-	if err := c.BindJSON(&qr); err != nil {
+	if err := c.BindJSON(&model); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "invalid payload."})
 		return
 	}
 
-	qrImage, err := generateQR(qr.URL)
+	qr, err := generateQR(model.URL)
 
 	if err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err})
 		return
 	}
 
-	c.IndentedJSON(http.StatusCreated, gin.H{"image": qrImage})
+	model.QRCode = qr
+	addQRToDb(&model)
+	c.IndentedJSON(http.StatusCreated, model)
 }
 
 func generateQR(url string) ([]byte, error) {
-	png, err := qrcode.Encode(url, qrcode.Medium, 256)
+	qr, err := qrcode.Encode(url, qrcode.Medium, 256)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return png, nil
+	return qr, nil
+}
+
+func addQRToDb(model *qrModel) {
+	db.Create(model)
+}
+
+func connectDB() error {
+	conn := "host=database-qrcode.c1j6msw5gk6k.us-east-1.rds.amazonaws.com user=postgres password=postgres dbname=database-qrcode port=5432"
+	var err error
+	db, err = gorm.Open(postgres.Open(conn), &gorm.Config{})
+
+	if err != nil {
+		return err
+	}
+
+	db.AutoMigrate(&qrModel{})
+	return nil
 }
 
 /*
